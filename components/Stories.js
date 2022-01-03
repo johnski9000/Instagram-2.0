04f4d6e71ab78f9dc19a30b714d35e9d4036329b
John <Januszwozniak.bth@gmail.com>
2022-01-03 10:06:21 +0000

    removed faker api for vercel deployment

--- components/Stories.js
+++ components/Stories.js
@@ -1,4 +1,4 @@
-import faker from "faker";
+// import faker from "faker";
 import React, { useEffect, useState } from 'react';
 import Story from "./Story";
 import Image from 'next/image'
@@ -8,17 +8,16 @@ function Stories() {
     const {data: session} = useSession();
     const [suggestions, setSuggestions ] = useState([]);
 
-    useEffect(() => {
-        // empty dependency below means that the code with fire as soon as the website loads
-        // only needs to be used once in order to populate the story data
-        const suggestions = [...Array(20)].map((_,
-             i) => ({
-            ...faker.helpers.contextualCard(),
-            id: i,
-        }));
-        setSuggestions(suggestions)
-        console.log(suggestions)
-    }, []);
+    // useEffect(() => {
+    //     // empty dependency below means that the code with fire as soon as the website loads
+    //     // only needs to be used once in order to populate the story data
+    //     const suggestions = [...Array(20)].map((_,
+    //          i) => ({
+    //         ...faker.helpers.contextualCard(),
+    //         id: i,
+    //     }));
+    //     setSuggestions(suggestions)
+    // }, []);
 
     return (
         <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll">
@@ -28,14 +27,14 @@ function Stories() {
                 username={session?.user.name}
                 />
             )}
-            {suggestions.map((profile) => (
+            {/* {suggestions.map((profile) => (
                 <Story
                 key={profile.id}
                 img={profile.img}
                 username={profile.username}
                 priority
                 />
-            ))}
+            ))} */}
         </div>
     );
 }
--- components/Suggestions.js
+++ components/Suggestions.js
@@ -1,19 +1,19 @@
 import { useEffect, useState} from "react"
-import faker from "faker"
+// import faker from "faker"
 
 function Suggestions() {
     const [suggestions, setSuggestions] = useState([]);
 
-    useEffect(() => {
-        const suggestions = [...Array(5)].map((_, i) => (
-            {
-                ...faker.helpers.contextualCard(),
-                id: i,
-            }
-        ))
-        setSuggestions(suggestions)
-        console.log(suggestions)
-    }, [])
+    // useEffect(() => {
+    //     const suggestions = [...Array(5)].map((_, i) => (
+    //         {
+    //             ...faker.helpers.contextualCard(),
+    //             id: i,
+    //         }
+    //     ))
+    //     setSuggestions(suggestions)
+    //     console.log(suggestions)
+    // }, [])
 
     return (
         <div className="mt-4 ml-10">
